6338bcf0a896fbaf1d629adb9a4d5a2072d00d6a
mousmasse <mousmasse@gmail.com>
2018-09-26 20:30:27 +0000

    test commit4

---
+++ src/main/java/sn/objis/gestionnumerocomptebancaire/presentation/Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package sn.objis.gestionnumerocomptebancaire.presentation;
+
+/**
+ * @author AMP
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test gestion compte bancaire");
+
+	}
+
+}
